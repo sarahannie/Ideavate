38cbcc0b9ea052cab4a095a1b8d18e5b2227fa01
sarahannie <sarahndianekwu@gmail.com>
2024-12-30 13:37:48 +0100

    start about in landing page

--- src/Landingpages/landingpage/landpage.jsx
+++ src/Landingpages/landingpage/landpage.jsx
@@ -40,7 +40,34 @@ const Landpage = () => {
   />
 </div>
     
-  {/* </div> */}
+{/* About Section */}
+<section className=" body-font">
+  <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+    <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+      <h3 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-green">
+      About ideavate?
+      </h3>
+      <h1 className="mb-8 text-dark sm:text-6xl text-4xl leading-relaxed">
+     
+Empowering <span className='text-green'>Ideas</span>  &
+<span className='text-green'> Connecting </span>Innovators
+and Investors
+      </h1>
+      <div className="flex justify-center">
+        <a href='' className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+          Read More
+        </a>
+      </div>
+    </div>
+    <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+      <img
+        className="object-cover object-center rounded"
+        alt="hero"
+        src="https://dummyimage.com/720x600"
+      />
+    </div>
+  </div>
+</section>
  
 
 
