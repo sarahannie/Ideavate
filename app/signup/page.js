b12a41c3726eac18be8edf0fc8254c6356cc9d4b
sarahannie <sarahndianekwu@gmail.com>
2025-01-06 11:24:10 +0100

    started signup page

---
+++ app/signup/page.js
@@ -0,0 +1,9 @@
+import Signup from "@/src/Landingpages/layout/signup";
+
+export default function Signups() {
+    return (
+      <div className="">
+      <Signup />
+      </div>
+    );
+  }
---
+++ src/Landingpages/layout/signup.jsx
@@ -0,0 +1,104 @@
+'use client'
+
+import { useState } from 'react'
+import { useRouter } from 'next/navigation'
+import { stringify } from 'postcss'
+import React from 'react'
+
+const Signup = () => {
+
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [error, setError] = useState('')
+  const router = useRouter()
+
+    const handleSubmit = async(e) =>{
+        e.preventDefault()
+        try{
+            const response = await fetch('/api/auth/signup',{
+                method :'POST',
+                headers:{'Content-Type':'application/json'},
+                body:JSON-stringify({name, email, password})
+            })
+            if(response.ok){
+                    router.push('/sigin')
+            }else{
+                const data = await response.json()
+                setError(data.message)
+            }
+        }catch{
+            setError('An error occurred during sign-up')
+        }
+    } 
+  return (
+    <form onSubmit={handleSubmit} className="mt-8 space-y-6">
+    <div className="rounded-md shadow-sm -space-y-px">
+      <div>
+        <label htmlFor="name" className="sr-only">
+          Name
+        </label>
+        <input
+          id="name"
+          name="name"
+          type="text"
+          required
+          className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+          placeholder="Name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="email" className="sr-only">
+          Email address
+        </label>
+        <input
+          id="email"
+          name="email"
+          type="email"
+          autoComplete="email"
+          required
+          className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+          placeholder="Email address"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="password" className="sr-only">
+          PasswordPassword
+        </label>
+        <input
+          id="password"
+          name="password"
+          type="password"
+          autoComplete="new-password"
+          required
+          className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+    </div>
+
+    <div>
+      <button
+        type="submit"
+        className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+      >
+        Sign up
+      </button>
+    </div>
+
+    {error && (
+      <div className="mt-4 text-center text-sm text-red-600">
+        {error}
+      </div>
+    )}
+  </form>
+  )
+}
+
+export default Signup
