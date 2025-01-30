5a1828398d6547dc9253eeffe2b9d312cebaf5a7
sarahannie <sarahndianekwu@gmail.com>
2025-01-30 05:51:57 +0100

    updated the suspense

--- app/signin/page.js
+++ app/signin/page.js
@@ -1,4 +1,5 @@
 import Signin from "@/src/Landingpages/layout/signin";
+import { Suspense } from "react"
 export const metadata = {
   title: 'Sign In',
   description: 'Sign in to your account',
@@ -7,7 +8,9 @@ export const metadata = {
 export default function Signups() {
     return (
       <div className="">
-      <Signin/>
+      <Suspense fallback={<div>Loading...</div>}>
+          <Signin/>
+      </Suspense>
       </div>
     );
   }
